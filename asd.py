6bbec47eff585a0366a42099e72b8aaac610b2fc
Sebastian <jsalvaradog@outlook.com>
2020-08-18 12:23:54 -0500

    try commit

---
+++ asd.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+a=9
+print "{:1d}".format(a)
+print "{:01d}".format(a)
+print "{:2d}".format(a)
+print "{:02d}".format(a)
+print "{:3d}".format(a)
+print "{:03d}".format(a)
+print "{:4d}".format(a)
+print "{:04d}".format(a)
+print "{:5d}".format(a)
+print "{:05d}".format(a)
+
